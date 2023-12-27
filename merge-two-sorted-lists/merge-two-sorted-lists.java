3c3318a08d8d9b27fd9e8c748ff432808a9bf803
Sunidhi Mistry <63811206+sunidhimm235@users.noreply.github.com>
2023-12-27 17:40:02 -0500

    Time: 1 ms, Memory: 42.4 MB - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.java
+++ merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -20,7 +20,6 @@ class Solution {
             return list1;
         
         ListNode result;
-        
         if (list1.val <= list2.val)
         {
             result = new ListNode(list1.val);
@@ -34,39 +33,32 @@ class Solution {
         
         ListNode temp = result;
         
-        while (list1 != null && list2 != null)
+        while (list1 != null || list2 != null)
         {
-            System.out.println(list1.val + " " + list2.val);
-            
-            if (list1.val <= list2.val)
+            if (list1 != null && list2 != null)
             {
-                System.out.println("First Condition");
+                if (list1.val <= list2.val)
+                {
+                    temp.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }
+                else
+                {
+                    temp.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                }
+            }
+            else if (list1 != null)
+            {
                 temp.next = new ListNode(list1.val);
                 list1 = list1.next;
             }
             else if (list2 != null)
             {
-                System.out.println("Second Condition");
                 temp.next = new ListNode(list2.val);
                 list2 = list2.next;
             }
             
-            temp = temp.next;
-        }
-        
-        while (list1 != null)
-        {
-            System.out.println("First Loop");
-            temp.next = new ListNode(list1.val);
-            list1 = list1.next;
-            temp = temp.next;
-        }
-        
-        while (list2 != null)
-        {
-            System.out.println("Second Loop: " + list2.val);
-            temp.next = new ListNode(list2.val);
-            list2 = list2.next;
             temp = temp.next;
         }
         
